08c6693e807c2acabeb76e555242c5711ba012eb
Syntaf <gmercer015@gmail.com>
2015-07-19 11:07:26 -0400

    BigNum::new no longer allocates a new string

--- src/lib.rs
+++ src/lib.rs
@@ -34,14 +34,14 @@ impl<'a> Sub for &'a BigNum {
     type Output = BigNum;
 
     fn sub(self, op: &'a BigNum) -> BigNum {
-        BigNum::new("12345678".to_string())
+        BigNum::new("12345678")
     }
 }
 
 
 impl BigNum {
-    pub fn new<S>(t_num: S) -> BigNum where S: Into<String> {
-        let filter_vec = t_num.into().chars().
+    pub fn new(t_num: &str) -> BigNum {
+        let filter_vec = t_num.chars().
             filter_map(|a| a.to_digit(10)).
             collect::<Vec<_>>();
         let t_digits = filter_vec.len();
