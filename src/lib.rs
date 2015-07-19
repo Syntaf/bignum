6d38a40e72a351289d099bba14dd0c46936080b1
Syntaf <gmercer015@gmail.com>
2015-07-19 11:04:22 -0400

    ops traits now implemented under &BigNum

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,40 @@
 use std::ops::{Add, Sub};
+use std::fmt::{Display, Formatter, Error};
 
 pub struct BigNum {
     value: String,
     digits: usize
 }
 
-impl Add for BigNum {
+impl Display for BigNum {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}", self.value)
+    }
+}
+
+
+impl<'a> Add for &'a BigNum {
     type Output = BigNum;
 
-    pub fn add(&self, op: &BigNum) -> BigNum {
+    fn add(self, op: &'a BigNum) -> BigNum {
         BigNum::new("123456".to_string())
     }
 }
 
-impl Sub for BigNum {
+/*
+impl<'b> Sub for &'b BigNum {
     type Output = BigNum;
 
-    pub fn sub(&self, op: &BigNum) -> BigNum {
+    fn sub(self, op: BigNum) -> BigNum {
         BigNum::new("12345678".to_string())
     }
 }
+*/
 
 impl BigNum {
     pub fn new<S>(t_num: S) -> BigNum where S: Into<String> {
-        BigNum { value: t_num, digits: t_num.len }
+        let t_value = t_num.into();
+        let t_digits = t_value.len();
+        BigNum { value: t_value, digits: t_digits }
     }
 }
