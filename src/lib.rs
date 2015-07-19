d69b38a1d22751a914b3633dfaa1e9f592d8f2b1
Syntaf <gmercer015@gmail.com>
2015-07-19 11:06:47 -0400

    refactored internal structure and began add impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 use std::ops::{Add, Sub};
 use std::fmt::{Display, Formatter, Error};
+use std::cmp::max;
+use std::iter::Zip;
 
 pub struct BigNum {
-    value: String,
+    raw: Vec<u32>,
     digits: usize
 }
 
 impl Display for BigNum {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "{}", self.value)
+        write!(f, "{:?}", self.raw)
     }
 }
 
@@ -17,7 +19,13 @@ impl<'a> Add for &'a BigNum {
     type Output = BigNum;
 
     fn add(self, op: &'a BigNum) -> BigNum {
-        BigNum::new("123456".to_string())
+        let ciel = max(self.digits, op.digits);
+        let mut carry = 0;
+        for (x, y) in self.raw.iter().zip(op.raw.iter()) {
+            println!("{}, {}", x, y);
+        }
+
+        BigNum::new("12345")
     }
 }
 
@@ -33,8 +41,10 @@ impl<'a> Sub for &'a BigNum {
 
 impl BigNum {
     pub fn new<S>(t_num: S) -> BigNum where S: Into<String> {
-        let t_value = t_num.into();
-        let t_digits = t_value.len();
-        BigNum { value: t_value, digits: t_digits }
+        let filter_vec = t_num.into().chars().
+            filter_map(|a| a.to_digit(10)).
+            collect::<Vec<_>>();
+        let t_digits = filter_vec.len();
+        BigNum { raw: filter_vec, digits: t_digits }
     }
 }
