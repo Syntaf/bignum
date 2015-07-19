a0fb99a1fff14a7f67e12f66ddd77eadc8e52cd5
Syntaf <gmercer015@gmail.com>
2015-07-19 11:03:03 -0400

    initial structure of bignum struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-#[test]
-fn it_works() {
+use std::ops::{Add, Sub};
+
+pub struct BigNum {
+    value: String,
+    digits: usize
+}
+
+impl Add for BigNum {
+    type Output = BigNum;
+
+    pub fn add(&self, op: &BigNum) -> BigNum {
+        BigNum::new("123456".to_string())
+    }
+}
+
+impl Sub for BigNum {
+    type Output = BigNum;
+
+    pub fn sub(&self, op: &BigNum) -> BigNum {
+        BigNum::new("12345678".to_string())
+    }
+}
+
+impl BigNum {
+    pub fn new<S>(t_num: S) -> BigNum where S: Into<String> {
+        BigNum { value: t_num, digits: t_num.len }
+    }
 }
---
+++ src/unit.rs
@@ -0,0 +1,4 @@
+[#test]
+fn printing() {
+    println!("testing");
+}
