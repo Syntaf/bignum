f8e7d02b3a2c420e78154c6715017179f7099cfe
Syntaf <gmercer015@gmail.com>
2015-08-07 15:06:52 -0400

    Wrote subtraction tests

--- tests/subtraction.rs
+++ tests/subtraction.rs
@@ -1,5 +1,7 @@
 extern crate bignum;
 
+use std::str::FromStr;
+
 #[test]
 fn subtract_no_borrow() {
     let a = bignum::BigNum::from_u32(666666666);
@@ -19,6 +21,17 @@ fn subtract_with_many_borrow() {
     let a = bignum::BigNum::from_u32(900045);
     let b = bignum::BigNum::from_u32(52);
     assert_eq!(&a - &b, bignum::BigNum::from_u32(899993));
+}
+
+#[test]
+fn subtract_with_varying_lengths() {
+    let a = bignum::BigNum::from_u32(20000);
+    let b = bignum::BigNum::from_u32(100);
+    assert_eq!(&a - &b, bignum::BigNum::from_u32(19900));
+
+    let c = bignum::BigNum::from_u32(192);
+    let d = bignum::BigNum::from_u32(7);
+    assert_eq!(&c - &d, bignum::BigNum::from_u32(185));
 }
 
 #[test]
@@ -42,6 +55,15 @@ fn negative_result_subtract2() {
 #[test]
 #[should_panic]
 fn negative_result_subtract3() {
+    let a = bignum::BigNum::from_str("121294838759").unwrap();
+    let b = bignum::BigNum::from_str("121394838759").unwrap();
+
+    let _c = &a - &b;
+}
+
+#[test]
+#[should_panic]
+fn negative_result_subtract4() {
     let a = bignum::BigNum::from_u32(451245);
     let b = bignum::BigNum::from_u32(461245);
 
